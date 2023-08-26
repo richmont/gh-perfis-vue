cd3c7df49532d01e1f779fddcb400137a74af373
Richelmy <richelmy.monteiro@gmail.com>
2023-08-26 13:55:56 -0300

    Tamanho do avatar definido para 100px

--- src/components/icons/UserAvatar.vue
+++ src/components/icons/UserAvatar.vue
@@ -8,7 +8,7 @@ defineProps(
 </script>
 <template>
     <div class="user-avatar">
-        <img v-bind:src="avatar_url" />
+        <img v-bind:src="avatar_url" :width="100" :height="100" />
     </div>
 
 </template>

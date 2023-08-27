3204c65b49aa2a02288a07cd1ccbc1d73cd45130
Richelmy <richelmy.monteiro@gmail.com>
2023-08-27 18:43:50 -0300

    Exibindo componentes de repositórios, array vazio precisa ser acessado no indice 0 para ver conteudo

--- src/views/RepositoryView.vue
+++ src/views/RepositoryView.vue
@@ -12,11 +12,14 @@ const usernameGithub = ref('')
 var userData = reactive({})
 
 const apiReposUrl = ref()
-let repoData = reactive([{}])
+const repoData = ref([])
 
 
 function getRepoData(url){
-repoData = [
+  
+  if (url == "https://api.github.com/users/richmont/repos"){
+  let repoJson = [
+  
   {
     "id": 559363474,
     "node_id": "R_kgDOIVc1kg",
@@ -113,13 +116,128 @@ repoData = [
     "topics": [
 
     ],
+    "visibility": "public",
+    "forks": 0,
+    "open_issues": 0,
+    "watchers": 0,
+    "default_branch": "main"
+  },
+  {
+    "id": 188931428,
+    "node_id": "MDEwOlJlcG9zaXRvcnkxODg5MzE0Mjg=",
+    "name": "clappr-android",
+    "full_name": "richmont/clappr-android",
+    "private": false,
+    "owner": {
+      "login": "richmont",
+      "id": 8619725,
+      "node_id": "MDQ6VXNlcjg2MTk3MjU=",
+      "avatar_url": "https://avatars.githubusercontent.com/u/8619725?v=4",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/richmont",
+      "html_url": "https://github.com/richmont",
+      "followers_url": "https://api.github.com/users/richmont/followers",
+      "following_url": "https://api.github.com/users/richmont/following{/other_user}",
+      "gists_url": "https://api.github.com/users/richmont/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/richmont/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/richmont/subscriptions",
+      "organizations_url": "https://api.github.com/users/richmont/orgs",
+      "repos_url": "https://api.github.com/users/richmont/repos",
+      "events_url": "https://api.github.com/users/richmont/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/richmont/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    "html_url": "https://github.com/richmont/clappr-android",
+    "description": "An extensible media player for Android.",
+    "fork": true,
+    "url": "https://api.github.com/repos/richmont/clappr-android",
+    "forks_url": "https://api.github.com/repos/richmont/clappr-android/forks",
+    "keys_url": "https://api.github.com/repos/richmont/clappr-android/keys{/key_id}",
+    "collaborators_url": "https://api.github.com/repos/richmont/clappr-android/collaborators{/collaborator}",
+    "teams_url": "https://api.github.com/repos/richmont/clappr-android/teams",
+    "hooks_url": "https://api.github.com/repos/richmont/clappr-android/hooks",
+    "issue_events_url": "https://api.github.com/repos/richmont/clappr-android/issues/events{/number}",
+    "events_url": "https://api.github.com/repos/richmont/clappr-android/events",
+    "assignees_url": "https://api.github.com/repos/richmont/clappr-android/assignees{/user}",
+    "branches_url": "https://api.github.com/repos/richmont/clappr-android/branches{/branch}",
+    "tags_url": "https://api.github.com/repos/richmont/clappr-android/tags",
+    "blobs_url": "https://api.github.com/repos/richmont/clappr-android/git/blobs{/sha}",
+    "git_tags_url": "https://api.github.com/repos/richmont/clappr-android/git/tags{/sha}",
+    "git_refs_url": "https://api.github.com/repos/richmont/clappr-android/git/refs{/sha}",
+    "trees_url": "https://api.github.com/repos/richmont/clappr-android/git/trees{/sha}",
+    "statuses_url": "https://api.github.com/repos/richmont/clappr-android/statuses/{sha}",
+    "languages_url": "https://api.github.com/repos/richmont/clappr-android/languages",
+    "stargazers_url": "https://api.github.com/repos/richmont/clappr-android/stargazers",
+    "contributors_url": "https://api.github.com/repos/richmont/clappr-android/contributors",
+    "subscribers_url": "https://api.github.com/repos/richmont/clappr-android/subscribers",
+    "subscription_url": "https://api.github.com/repos/richmont/clappr-android/subscription",
+    "commits_url": "https://api.github.com/repos/richmont/clappr-android/commits{/sha}",
+    "git_commits_url": "https://api.github.com/repos/richmont/clappr-android/git/commits{/sha}",
+    "comments_url": "https://api.github.com/repos/richmont/clappr-android/comments{/number}",
+    "issue_comment_url": "https://api.github.com/repos/richmont/clappr-android/issues/comments{/number}",
+    "contents_url": "https://api.github.com/repos/richmont/clappr-android/contents/{+path}",
+    "compare_url": "https://api.github.com/repos/richmont/clappr-android/compare/{base}...{head}",
+    "merges_url": "https://api.github.com/repos/richmont/clappr-android/merges",
+    "archive_url": "https://api.github.com/repos/richmont/clappr-android/{archive_format}{/ref}",
+    "downloads_url": "https://api.github.com/repos/richmont/clappr-android/downloads",
+    "issues_url": "https://api.github.com/repos/richmont/clappr-android/issues{/number}",
+    "pulls_url": "https://api.github.com/repos/richmont/clappr-android/pulls{/number}",
+    "milestones_url": "https://api.github.com/repos/richmont/clappr-android/milestones{/number}",
+    "notifications_url": "https://api.github.com/repos/richmont/clappr-android/notifications{?since,all,participating}",
+    "labels_url": "https://api.github.com/repos/richmont/clappr-android/labels{/name}",
+    "releases_url": "https://api.github.com/repos/richmont/clappr-android/releases{/id}",
+    "deployments_url": "https://api.github.com/repos/richmont/clappr-android/deployments",
+    "created_at": "2019-05-28T01:26:19Z",
+    "updated_at": "2019-05-28T01:26:22Z",
+    "pushed_at": "2019-05-23T18:27:51Z",
+    "git_url": "git://github.com/richmont/clappr-android.git",
+    "ssh_url": "git@github.com:richmont/clappr-android.git",
+    "clone_url": "https://github.com/richmont/clappr-android.git",
+    "svn_url": "https://github.com/richmont/clappr-android",
+    "homepage": null,
+    "size": 1919,
+    "stargazers_count": 0,
+    "watchers_count": 0,
+    "language": "Kotlin",
+    "has_issues": false,
+    "has_projects": true,
+    "has_downloads": true,
+    "has_wiki": true,
+    "has_pages": false,
+    "has_discussions": false,
+    "forks_count": 0,
+    "mirror_url": null,
+    "archived": false,
+    "disabled": false,
+    "open_issues_count": 0,
+    "license": {
+      "key": "bsd-3-clause",
+      "name": "BSD 3-Clause \"New\" or \"Revised\" License",
+      "spdx_id": "BSD-3-Clause",
+      "url": "https://api.github.com/licenses/bsd-3-clause",
+      "node_id": "MDc6TGljZW5zZTU="
+    },
+    "allow_forking": true,
+    "is_template": false,
+    "web_commit_signoff_required": false,
+    "topics": [
 
+    ],
+    "visibility": "public",
+    "forks": 0,
+    "open_issues": 0,
+    "watchers": 0,
+    "default_branch": "dev"
   }]
+    repoData.value.push(repoJson)
+  } else{
+    console.log("Repos invalidos")
+  }
 }
 
 function submitUsername(usuario){
   if (usuario=="richmont"){
-    console.log(usuario)
     let jsonUser =  {
       "login": "richmont",
       "id": 8619725,
@@ -171,7 +289,8 @@ function submitUsername(usuario){
     apiReposUrl.value = userData.repos_url
     getRepoData(apiReposUrl.value)
 }else{
-  userData.id = null
+  // limpa os atributos da variável
+    userData.id = null
     userData.login = null
     userData.avatar_url = null
     userData.url = null
@@ -192,11 +311,11 @@ function submitUsername(usuario){
 
 <template>
     <div class="input-username">
-        <input v-model="usernameGithub" placeholder="Type github username">
-        <button @click.prevent="submitUsername(usernameGithub);">Consultar</button>
+        <input @keydown.enter="submitUsername(usernameGithub)" v-model="usernameGithub" placeholder="Type github username">
+        <button @click="submitUsername(usernameGithub);">Consultar</button>
         
     </div>
-    <div class="user-data" v-if="userData.id != undefined">
+    <div class="user-data" v-if="userData.id != null">
         <UserItem 
         :id="userData.id"
         :login="userData.login"
@@ -209,6 +328,10 @@ function submitUsername(usuario){
         :description="userData.description"
         />
     </div>
-
+    
+    <div v-if="repoData.length > 0">
+      
+      <RepositoryList :repoList="repoData[0]"/>
+    </div>
 
 </template>
